067dbadf98d83948204f0f41dfbba68a8961041d
eduardo_dorado <eduardo.dorado@sanmina.com>
2022-11-02 13:22:38 -0600

    update - change commonjs to modulejs

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-const express = require('express');
-require('dotenv').config();
-const cors = require('cors');
+import express from'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
 
+dotenv.config();
+
+// App
 const app = express();
 const PORT = process.env.PORT || 4000;
-app.use( cors() );
 
+// Middlewares
+app.use( cors() );
 app.use( express.json() );
 
+// Routers
 app.get('/', (req, res) => {
     res.json({
         msg: 'Message from / route'
